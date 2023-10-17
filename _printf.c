f1466701ed5c140ba82aab5c6a6eabf1e7a59e67
ukemeikot <ukemeetim2222@gmail.com>
2023-10-17 21:02:00 +0100

    ukeme

--- _printf.c
+++ _printf.c
@@ -34,12 +34,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	}
 	buf_idx = string(format, my_func_list, arguments, buf, &buf_idx);
-<<<<<<< HEAD
-=======
-	buf[buf_idx + 1] = '\0';
->>>>>>> main
 	write(1, buf, buf_idx);
-	printf("%d\n", buf_idx);
 	va_end(arguments);
 	return (buf_idx);
 }
